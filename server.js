adbf1163a8f0609229d24a27e77779782b05aec8
Aaron Nobles <43255910+Aaron24000@users.noreply.github.com>
2021-03-01 01:30:55 -0500

    Still trying to fix deployment issue ugh

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
+const connectDB = require('./config/db');
 const path = require('path');
 
 const app = express();
@@ -7,21 +7,23 @@ const app = express();
 // Connect Database
 connectDB();
 
-// Init midlleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-// Server static assets in production
-if(process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
-
-//Define Routes
-app.use("/api/users", require("./routes/users"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
