f9b9d6a0236509f92b16eb5d000677e127ae5ae1
Aaron Nobles <43255910+Aaron24000@users.noreply.github.com>
2021-03-01 02:16:06 -0500

    I just wanna sleep at this point

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
-import ContactItem from '../contacts/ContactItem';
 import ContactFilter from '../contacts/ContactFilter';
 import AuthContext from '../../context/auth/authContext';
 
